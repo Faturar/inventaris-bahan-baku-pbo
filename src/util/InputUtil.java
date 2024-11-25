aeb499f7c3746f63de2fec7c7c53747404292b38
darwisyalfarizi <cmxdarwisy32@gmail.com>
2024-11-25 17:39:13 +0700

    init project

--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class InputUtil  {
 
     private static Scanner scanner = new Scanner(System.in);
-    public static String input(String info){
+    public static String inputS(String info){
         System.out.print(info +" : ");
         String data = scanner.nextLine();
+        return data;
+    }
+    public static Integer inputI(int angka){
+        Integer data = scanner.nextInt();
         return data;
     }
 
